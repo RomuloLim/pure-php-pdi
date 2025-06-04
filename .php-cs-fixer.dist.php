e29d23d699486d7a6183613a198f8f640af8d89d
RomuloLim <romulo.lf123@gmail.com>
2025-06-04 17:30:03 -0300

    feat: php-cs-fixer

---
+++ .php-cs-fixer.dist.php
@@ -0,0 +1,33 @@
+<?php
+declare(strict_types=1);
+
+$finder = (new PhpCsFixer\Finder())
+    ->exclude(['vendor'])
+    ->in(__DIR__);
+
+return (new PhpCsFixer\Config())
+    ->setRiskyAllowed(true)
+    ->setRules([
+        '@PSR12' => true,
+        'strict_param' => true,
+        'declare_strict_types' => true,
+        'array_syntax' => ['syntax' => 'short'],
+        'binary_operator_spaces' => [
+            'operators' => [
+                '=>' => 'align_double_arrow',
+                '=' => 'align_single_space_minimal'
+            ]
+        ],
+        'no_unused_imports' => true,
+        'ternary_operator_spaces' => true,
+        'trailing_comma_in_multiline' => [
+            'elements' => ['arrays', 'arguments', 'parameters']
+        ],
+        'global_namespace_import' => [
+            'import_classes' => true,
+            'import_constants' => true,
+            'import_functions' => true,
+        ],
+        'modernize_types_casting' => true,
+    ])
+    ->setFinder($finder);
--- public/index.php
+++ public/index.php
@@ -1,4 +1,7 @@
 <?php
+
+declare(strict_types=1);
+
 // @codeCoverageIgnoreStart
 echo 'This is the public/index.php file.';
 // @codeCoverageIgnoreEnd
