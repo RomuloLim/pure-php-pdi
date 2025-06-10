025e66f4f3188f8b100ae55ed2c2cd3984afcf3c
RomuloLim <romulo.lf123@gmail.com>
2025-06-10 18:08:04 -0300

    feat: gitignore and testing

--- public/index.php
+++ public/index.php
@@ -1,7 +1,10 @@
 <?php
 
 declare(strict_types=1);
+require __DIR__ . '/../vendor/autoload.php';
 
 // @codeCoverageIgnoreStart
+
 echo 'This is the public/index.php file.';
+
 // @codeCoverageIgnoreEnd
--- src/index.php
+++ src/index.php
@@ -4,4 +4,6 @@ declare(strict_types=1);
 
 // @codeCoverageIgnoreStart
 echo 'This is the public/index.php file.';
+
+ds('This file is used to handle incoming requests and route them to the appropriate controllers or services.');
 // @codeCoverageIgnoreEnd
