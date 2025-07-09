291aa63392d944d4af38ef2f4a9a6ffb89661bba
RomuloLim <romulo.lf123@gmail.com>
2025-07-09 18:11:09 -0300

    improv: turn document validator strategy readonly

--- src/Core/Validations/DocumentValidator.php
+++ src/Core/Validations/DocumentValidator.php
@@ -5,11 +5,10 @@ declare(strict_types=1);
 namespace App\Core\Validations;
 
 use App\Core\Validations\Contracts\DocumentValidatorStrategy;
-use App\Core\Validations\Enums\DocumentValidatorEnum;
 
 class DocumentValidator
 {
-    public function __construct(private DocumentValidatorStrategy $documentValidator){}
+    public function __construct(private readonly DocumentValidatorStrategy $documentValidator){}
 
     public function validate(string $document): bool
     {
